c787913dfaacb31fe3ef66c178c79afa71d34786
Maxtors <moe.andreas@gmail.com>
2015-11-28 12:04:00 +0100

    Added install of logstash and elasticsearch + download of kibana

--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@
 
 # Variables for this provisioning script
 GOLANGFILE="go1.5.1.linux-amd64.tar.gz"
+KIBANAFILE="kibana-4.3.0-linux-x64.tar.gz"
 
 function log {
     echo "[$(date)]: $1"
@@ -40,10 +41,34 @@ function safe_create {
     check_result $? "Something went wrong while creating: $1"
 }
 
+# Add Elastic key to the system
+rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
+
+# Add the Elasticsearch Repository to DNF
+echo "[elasticsearch-2.x]
+name=Elasticsearch repository for 2.x packages
+baseurl=http://packages.elastic.co/elasticsearch/2.x/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1" > /etc/yum.repos.d/elasticsearch.repo
+
+# Add the Logstash Repository to DNF
+echo "[logstash-2.1]
+name=Logstash repository for 2.1.x packages
+baseurl=http://packages.elastic.co/logstash/2.1/centos
+gpgcheck=1
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1" > /etc/yum.repos.d/logstash.repo
+
 # Update and install common tools
 log "Updating system and installing common tools"
 dnf --quiet --debuglevel=0 -y update &> /dev/null
-quiet_install wget git vim make libtool gcc docker
+quiet_install htop nload wget git vim make libtool gcc docker elasticsearch logstash redis
+
+# Enable Elasticsearch
+log "Enabeling Elasticsearch in systemctl"
+/bin/systemctl daemon-reload
+/bin/systemctl enable /usr/lib/systemd/system/elasticsearch.servicie
 
 # Start docker process
 log "Starting Docker process"
@@ -52,6 +77,21 @@ sudo systemctl start docker
 # Install dependencies for suricata
 log "Installing dependencies for Suricata"
 quiet_install GeoIP-devel hiredis-devel luajit-devel libpcap-devel pcre-devel libyaml-devel file-devel zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel
+
+# Download Kibana
+log "Downloading Kibana Archive"
+wget --quiet https://download.elastic.co/kibana/kibana/$KIBANAFILE -O /tmp/$KIBANAFILE
+
+# If the file was downloaded and all is good, then extract the archive
+if [ $? -eq 0 ] && [ -f /tmp/$KIBANAFILE ]; then
+
+    # Extract the kibana archive
+    tar -C /usr/local -xzf /tmp/$KIBANAFILE
+    check_result $? "Something went wrong while extracting /tmp/$KIBANAFILE"
+else
+    log "Cannot find $KIBANAFILE, was download not successful?"
+    exit 1
+fi
 
 # Download Go
 log "Downloading Go-Lang Archive"
@@ -62,8 +102,6 @@ if [ $? -eq 0 ] && [ -f /tmp/$GOLANGFILE ]; then
     
     # Extract the golang tar to /usr/local
     tar -C /usr/local -xzf /tmp/$GOLANGFILE
-
-    # Check if something went wrong with un-archiving
     check_result $? "Something went wrong while extracting /tmp/$GOLANGFILE"
 
     # Create golang workspace
@@ -76,7 +114,7 @@ if [ $? -eq 0 ] && [ -f /tmp/$GOLANGFILE ]; then
     safe_append "export PATH=$PATH:/usr/local/go/bin" "/home/vagrant/.bash_profile"
     safe_append "export GOPATH=/home/vagrant/go" "/home/vagrant/.bash_profile"
 else
-    log "Cannot find GOLANGFILE, was download not successful?"
+    log "Cannot find $GOLANGFILE, was download not successful?"
     exit 1
 fi
 
