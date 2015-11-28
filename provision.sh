0806b7891988d077aec26522ff3bc01b23358384
Maxtors <moe.andreas@gmail.com>
2015-11-28 11:32:49 +0100

    Reworked suricata build process and output

--- provision.sh
+++ provision.sh
@@ -5,13 +5,6 @@
 # Variables for this provisioning script
 GOLANGFILE="go1.5.1.linux-amd64.tar.gz"
 
-# Function to add something to a file if it is not present
-function add_if_not_there {
-    if ! grep -q "$1" "$2"; then
-        echo "$1" >> "$2"
-    fi
-}
-
 function log {
     echo "[$(date)]: $1"
 }
@@ -28,6 +21,25 @@ function quiet_install {
     fi
 }
 
+function check_result {
+    if [ ! $1 -eq 0 ]; then
+        log "$2"
+        exit 1
+    fi
+}
+
+function safe_append {
+    if ! grep -q "$1" "$2"; then
+        echo "$1" >> "$2"
+        check_result $? "Something whent wrong while adding content to $2"
+    fi
+}
+
+function safe_create {
+    [ -d $1 ] || mkdir $1
+    check_result $? "Something went wrong while creating: $1"
+}
+
 # Update and install common tools
 log "Updating system and installing common tools"
 dnf --quiet --debuglevel=0 -y update &> /dev/null
@@ -39,7 +51,7 @@ sudo systemctl start docker
 
 # Install dependencies for suricata
 log "Installing dependencies for Suricata"
-quiet_install hiredis-devel luajit-devel libpcap-devel pcre-devel libyaml-devel file-devel zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel
+quiet_install GeoIP-devel hiredis-devel luajit-devel libpcap-devel pcre-devel libyaml-devel file-devel zlib-devel jansson-devel nss-devel libcap-ng-devel libnet-devel
 
 # Download Go
 log "Downloading Go-Lang Archive"
@@ -52,18 +64,17 @@ if [ $? -eq 0 ] && [ -f /tmp/$GOLANGFILE ]; then
     tar -C /usr/local -xzf /tmp/$GOLANGFILE
 
     # Check if something went wrong with un-archiving
-    if [ ! $? -eq 0 ]; then
-        log "Something went wrong while extracting $GOLANGFILE"
-        exit 1
-    fi
+    check_result $? "Something went wrong while extracting /tmp/$GOLANGFILE"
 
-    # Add ENV variables and create workspace
-    add_if_not_there "export PATH=$PATH:/usr/local/go/bin" "/home/vagrant/.bash_profile"
-    [ -d /home/vagrant/go ] || mkdir /home/vagrant/go
-    [ -d /home/vagrant/go/src ] || mkdir /home/vagrant/go/src
-    [ -d /home/vagrant/go/pkg ] || mkdir /home/vagrant/go/pkg
-    [ -d /home/vagrant/go/bin ] || mkdir /home/vagrant/go/bin
-    add_if_not_there "export GOPATH=/home/vagrant/go" "/home/vagrant/.bash_profile"
+    # Create golang workspace
+    safe_create /home/vagrant/go
+    safe_create /home/vagrant/go/src
+    safe_create /home/vagrant/go/pkg
+    safe_create /home/vagrant/go/bin
+    
+    # Add ENV variables (PATH and GOPATH)
+    safe_append "export PATH=$PATH:/usr/local/go/bin" "/home/vagrant/.bash_profile"
+    safe_append "export GOPATH=/home/vagrant/go" "/home/vagrant/.bash_profile"
 else
     log "Cannot find GOLANGFILE, was download not successful?"
     exit 1
@@ -72,23 +83,34 @@ fi
 
 # Create working area for Suricata installation
 log "Creating dev folder in Vagrant HOME"
-[ -d /home/vagrant/dev ] || mkdir /home/vagrant/dev
+safe_create /home/vagrant/dev
 
 # Clone Suricata GIT Repo
 log "Cloning Suricata GITHUB Repository"
 cd /home/vagrant/dev
+if [ -d suricata ]; then rm -rf suricata; fi
 git clone https://github.com/inliniac/suricata.git
 
 log "Cloning OISF LibHTP GITHUB Repository"
 cd suricata
+if [ -d libhtp ]; then rm -rf libhtp; fi
 git clone https://github.com/ironbee/libhtp
 
-# Configure and make suricata
-log "Configuring suricata and performing make + install"
-./autogen.sh
-./configure --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/ --enable-luajit --enable-hiredis --enable-lua --enable-luajit --enable-geoip
-make
-make install-full
+log "Running Suricata autogen"
+./autogen.sh > /tmp/suricata_autogen.log 2>&1
+check_result $? "Something when wrong when running autogen.sh: `cat /tmp/suricata_autogen.log`"
+
+log "Configuring Suricata"
+./configure --prefix=/usr/ --sysconfdir=/etc/ --localstatedir=/var/ --enable-luajit --enable-hiredis --enable-geoip > /tmp/suricata_configure.log 2>&1
+check_result $? "Something went wrong when trying to configure suricata: `cat /tmp/suricata_configure.log`"
+
+log "Making Suricata"
+make > /tmp/suricata_make.log 2>&1
+check_result $? "Something went wrong when trying to make suricata: `cat /tmp/suricata_make.log`"
+
+log "Installing Suricata"
+make install-full > /tmp/suricata_make_install.log 2>&1
+check_result $? "Something went wrong when trying to install suricata: `cat /tmp/suricata_make_install.log`"
 
 # Set the correct user permisions
 log "Setting ownership permisions"
